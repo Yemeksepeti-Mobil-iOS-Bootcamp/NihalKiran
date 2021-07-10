b6c3af9382c0e5fb4ec508ae9545455f85f64451
nihalkiran <kiran.nihal.34@hotmail.com>
2021-07-10 03:43:17 +0300

    added pageControl

---
+++ Week3/HW-Week-3/HW-Week-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-Week-3
+//
+//  Created by Nihal Kiran on 10.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
